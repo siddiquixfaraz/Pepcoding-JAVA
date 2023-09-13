93c2f378c997be78465ba803eb7dd2ccd1e9a45d
siddiquixfaraz <109731664+siddiquixfaraz@users.noreply.github.com>
2023-09-13 16:29:25 +0530

    upadated list

--- src/DSA1/gettingStarted/InverseOfNo.java
+++ src/DSA1/gettingStarted/InverseOfNo.java
@@ -1,5 +1,15 @@
 package DSA1.gettingStarted;
 
 public class InverseOfNo {
-
+    public static void main(String[] args) {
+        int num = 613254;
+        int temp = num;
+        int inverse  = 0;
+        for (int i = 1; i <= 6 ; i++) {
+            int rem = temp % 10;
+            inverse = inverse + i *((int)Math.pow(10,rem-1));
+            temp = temp/10;
+        }
+        System.out.println(inverse);
+    }
 }
